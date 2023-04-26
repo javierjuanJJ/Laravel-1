488a4033aa1aaca1ed27663a0e88bef84a962c6d
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-04-26 10:16:05 +0200

    Commit 1

---
+++ resources/views/home.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="" alt="" class="rounded-circle">
+        </div>
+        <div class="col-9">
+            <div class="col-9 pt-5">
+                <h1>
+                  freecodecamp
+                </h1>
+                <div class="d-flex">
+                    <div><strong class="pr-5">153</strong> posts</div>
+                    <div><strong class="pr-5">23k</strong> followers</div>
+                    <div><strong class="pr-5">212</strong> following</div>
+                </div>
+                <div class="pt-4 font-weight-bold">freeCodeGram</div>
+                <div>Biography</div>
+                <div><a href="#">www.freecodeGram.org</a> </div>
+            </div>
+        </div>
+        <div class="row pt-5">
+            <div class="col-4">
+                <img src="" alt="" class="w-100">
+            </div>
+            <div class="col-4">
+                <img src="" alt="" class="w-100">
+            </div>
+            <div class="col-4">
+                <img src="" alt="" class="w-100">
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
